63c72984de7f5c3e15ac09d03ff9547a1830d56b
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2020-02-04 21:08:26 -0300

    #DEVVAZE: Início do Frontend

---
+++ web/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Componente
+//Estado
+//Propriedade
+
+function Teste(){
+  return <h1>I love you bb</h1>
+}
+
+function App() {
+  return (
+    <Teste />
+  );
+}
+
+export default App;
