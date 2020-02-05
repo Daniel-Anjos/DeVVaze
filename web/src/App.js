5bfd265fe3a5daf65da8fa703a410cd788e3b8f4
Daniel dos Anjos <daniel.anjos.1989@gmail.com>
2020-02-04 22:43:15 -0300

    #DEVVAZE: Alterações para exemplos dos conceitos do React

--- web/src/App.js
+++ web/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-//Componente
-//Estado
-//Propriedade
-
-function Teste(){
-  return <h1>I love you bb</h1>
-}
+//Componente: Bloco isolado de HTML, CSS e JS que não interfere no restante da aplicação
+//Propriedade: Informações que um componente pai passa para o componente filho
+//Estado: Informação mantidas/manipuladas pelo componente (Conceito de Imutabilidade)
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function incrementCounter(){
+    setCounter(counter + 1 );
+    alert('Fala cmgo bb');
+  }
+
   return (
-    <Teste />
+    <>
+      <h1>Contador: {counter} </h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
